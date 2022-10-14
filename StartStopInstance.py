cd2df259a6fe1cff62a89c4c798f924a9cc145b6
swati1704 <97314146+swati1704@users.noreply.github.com>
2022-10-14 11:33:28 +0530

    Create StartStopInstance.py

---
+++ StartStopInstance.py
@@ -0,0 +1,20 @@
+import boto3
+
+# region EC2 instances are in
+region = 'us-east-1'
+ec2 = boto3.resource('ec2', region_name=region)
+
+
+tagname = "Name"
+tagvalue = "notJenkins"
+
+instances=ec2.instances.filter(Filters=[
+    {'Name': 'tag': [tagname], 'Values': [tagvalue]}
+])
+
+for instance in instances:
+    try:
+        instance.stop()
+        print(f'{instance} stopped')
+    except:
+        print(f'Error stopping {instance}')
